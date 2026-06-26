e534999913260f00ba9ea9afb02d9c46b74b6445
SantosIk <80635720+SantosIk@users.noreply.github.com>
2026-06-26 22:24:22 +0100

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: "./",
+    base: "/pedid_para_minha_vida/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
